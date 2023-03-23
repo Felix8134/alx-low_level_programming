fe3a41cc84e6d9d4f12fe21ba714e165110dbbea
Felix8134 <domnic8134@gmail.com>
2023-03-23 16:32:40 +0300

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul -for a digit (0 through 9)
+ * @c: first int
+ * @c: second int
+ * Return: Always 0 (Success)
+ */
+
+int mul(int a, int b)
+
+{
+	return (a * b);
+}
