86c1b8aca847383731017f2293cb183d5ed9846c
Felix8134 <domnic8134@gmail.com>
2023-03-27 18:33:51 +0300

    6-puts2

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,17 +8,12 @@
 
 void print_rev(char *s)
 {
-	char rev = s[0];
-	int counter = 0;
 	int i;
+	int count = 0;
 
-	while (s[counter] != '\0')
-		counter++;
-	for (i = 0; i < counter; i++)
-	{
-		counter--;
-		rev = s[i];
-		s[i] = s[counter];
-		s[counter] = rev;
-	}
+	while (i = 0 ; s[i] != '\0' ; i++)
+		count++;
+	while (i = count ; i >= 0 ; i--)
+		_putchar(s[i]);
+	_putchar('\n');
 }
