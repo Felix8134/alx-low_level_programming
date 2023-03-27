bf5f88137046f3246582737844d73c7a81aa1570
Felix8134 <domnic8134@gmail.com>
2023-03-27 21:02:38 +0300

    101-keygen

---
+++ 0x05-pointers_arrays_strings/100-main.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int nb;
+
+	printf("%d\n", nb);
+	printf("%d\n", nb);
+	printf("%d\n", nb);
+	printf("%d\n", nb);
+	printf("%d\n", nb);
+	printf("%d\n", nb);
+	printf("%d\n", nb);
+	return (0);
+}
