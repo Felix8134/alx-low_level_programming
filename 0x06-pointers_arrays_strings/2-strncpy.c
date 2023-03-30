c3959532bc09ad17ae3dd1b5d8420a447ff33d43
Felix8134 <domnic8134@gmail.com>
2023-03-30 08:50:15 +0300

    2-strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strncpy - copy a string
+ * @dest: enter int
+ * @src: enter int
+ * @n: enter int
+ *
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int x;
+
+	x = 0;
+	while (x < n && src[x] != '\0')
+	{
+		dest[x] = src[x];
+		x++;
+	}
+	while (x < n)
+	{
+		dest[x] = '\0';
+		x++;
+	}
+
+	return (dest);
+}
