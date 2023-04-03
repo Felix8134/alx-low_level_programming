f88fe36c3be89c719bd798b7bae045659d7500bc
Felix8134 <domnic8134@gmail.com>
2023-04-03 16:32:04 +0300

    1-memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -9,11 +9,14 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int x;
+	int x = 0;
+	int y = n;
 
-	for (x = 0; x < n; x++)
+	for (; x < y; x++)
 	{
 		dest[x] = src[x];
+		n--;
 	}
 	return (dest);
+
 }
