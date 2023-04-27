0296f9c44272e93cb03adcf3c81862ba7d223300
Felix8134 <domnic8134@gmail.com>
2023-04-27 14:45:57 +0300

    2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,24 +11,24 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-	char *a;
-	int b;
+	char *dup;
+	int len;
 	list_t *x;
 
 	x = malloc(sizeof(list_t));
 	if (x == NULL)
 		return (NULL);
-	a = stra(str);
-	if (a == NULL)
+	dup = strdup(str);
+	if (dup == NULL)
 	{
 		free(x);
 		return (NULL);
 	}
 
-	for (b = 0; str[b];)
-		b++;
-	x->str = a;
-	x->b = b;
+	for (len = 0; str[len];)
+		len++;
+	x->str = dup;
+	x->len = len;
 	x->next = *head;
 	*head = x;
 
