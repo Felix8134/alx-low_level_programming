9cdecbf7941febf06e56b35d00c0b72c415f7a80
Felix8134 <domnic8134@gmail.com>
2023-05-01 18:01:43 +0300

    0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_listint - ...
+ * @h: ...
+ * Return: ...
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t x = 0;
+	while (h)
+	{
+		x++;
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (x);
+}
